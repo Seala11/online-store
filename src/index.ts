24fab2bca9a0657d428da9162dac743027f0468d
Hanna Papova <seal.annuska@gmail.com>
2022-07-28 18:46:48 -0400

    feat: deploy project

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-require('normalize.css/normalize.css');
-
-import '@src/sass/main.scss';
-import App from '@src/components/app/app';
-import '@src/components/helpers/taskCheck';
-
-new App(document.body);
